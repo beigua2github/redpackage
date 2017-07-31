f90bd6d09bc3268b969dd9ffaee1e42749367c19
huangzheng <huangzheng@smzdm.com>
2017-07-31 16:39:31 +0800

    实时计算红包大小

---
+++ redpackage-core/src/main/java/org/beigua/redpackage/real/RealTimeMain.java
@@ -0,0 +1,11 @@
+package org.beigua.redpackage.real;
+
+/**
+ * Created by smzdm on 2017/7/31.
+ */
+public class RealTimeMain {
+    //用乐观锁实时计算红包金额
+    public static void main(String[] args) {
+        //todo
+    }
+}
